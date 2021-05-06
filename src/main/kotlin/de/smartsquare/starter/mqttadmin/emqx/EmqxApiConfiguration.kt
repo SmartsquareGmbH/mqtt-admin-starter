846d4ccb9b452a13c3511b4b8492878935ccb068
Ruben Gees <rubengees7@googlemail.com>
2021-05-06 11:07:48 +0200

    Hide RestTemplate bean for consumers

--- src/main/kotlin/de/smartsquare/starter/mqttadmin/emqx/EmqxApiConfiguration.kt
+++ src/main/kotlin/de/smartsquare/starter/mqttadmin/emqx/EmqxApiConfiguration.kt
@@ -3,14 +3,12 @@ package de.smartsquare.starter.mqttadmin.emqx
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import de.smartsquare.starter.mqttadmin.client.BrokerApiClient
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
-import org.springframework.web.client.RestTemplate
 
 @Configuration
 @ConditionalOnProperty(prefix = "emqx.api", value = ["host"])
@@ -18,19 +16,17 @@ import org.springframework.web.client.RestTemplate
 class EmqxApiConfiguration {
 
     @Bean
-    @Qualifier("emqx")
-    fun restTemplate(config: EmqxApiProperties): RestTemplate {
+    fun emqxApiClient(config: EmqxApiProperties): BrokerApiClient {
         val objectMapper = jacksonObjectMapper()
             .findAndRegisterModules()
             .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
 
-        return RestTemplateBuilder()
+        val restTemplate = RestTemplateBuilder()
             .rootUri("${config.schema}://${config.host}:${config.port}")
             .basicAuthentication(config.username, config.password)
             .messageConverters(MappingJackson2HttpMessageConverter(objectMapper))
             .build()
-    }
 
-    @Bean
-    fun emqxApiClient(@Qualifier("emqx") restTemplate: RestTemplate): BrokerApiClient = EmqxApiClient(restTemplate)
+        return EmqxApiClient(restTemplate)
+    }
 }
