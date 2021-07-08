ff9a45620b06c3d8e4749b21a4462536ad3e5200
Ruben Gees <rubengees7@googlemail.com>
2021-07-08 13:46:37 +0200

    Generate configuration metadata

--- src/main/kotlin/de/smartsquare/starter/mqttadmin/emqx/EmqxApiProperties.kt
+++ src/main/kotlin/de/smartsquare/starter/mqttadmin/emqx/EmqxApiProperties.kt
@@ -2,13 +2,35 @@ package de.smartsquare.starter.mqttadmin.emqx
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.validation.annotation.Validated
 
-@ConfigurationProperties(prefix = "emqx.api")
+@Validated
 @ConstructorBinding
+@ConfigurationProperties(prefix = "emqx.api")
 data class EmqxApiProperties(
+
+    /**
+     * The schema to connect to.
+     */
     val schema: String = "http",
+
+    /**
+     * The host to connect to.
+     */
     val host: String,
+
+    /**
+     * The port to connect to.
+     */
     val port: String,
+
+    /**
+     * The username to use.
+     */
     val username: String,
+
+    /**
+     * The password to use.
+     */
     val password: String
 )
